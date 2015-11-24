b13e694908f29385f88d2222accac564cdd7ad9c
Erin Bell <euclid@Erin-Bells-MacBook-Pro.local>
2015-11-24 14:08:05 -0500

    adds event notifications

--- CalendarFeedPlugin.php
+++ CalendarFeedPlugin.php
@@ -69,14 +69,79 @@ function cf_calendar_scripts($option=null)
 	$feed=get_option('cf_rssfeed');
 	$show_mobile=get_option('cf_displayOnMobile');
 	$breakpoint='768';
-
-	$html  = '<script async defer>console.log("'.$feed.'")</script>';
-
-	if(!$show_mobile){
-		//TODO: swap out the boolean option for a user-configurable min breakpoint where 0 equals none
-		$html.='<style type="text/css">@media all and (max-width:'.$breakpoint.'px){
-			#notify#calendar{display:none !important;}}</style>';
+	?>
+	
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script type="text/javascript">google.load("feeds", "1");</script>            
+	<script>
+	function stripHTML(dirtyString) {
+	    var container = document.createElement('div');
+	    container.innerHTML = dirtyString;
+	    return container.textContent || container.innerText;
 	}
-	return $html;
+
+	function setCookie(cname, cvalue, exdays) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0; i<ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1);
+	        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+	    }
+	    return "";
+	}
+        	
+	jQuery(jQuery(window)).load(function(){
+	    var feed = new google.feeds.Feed('<?php echo $feed;?>');
+	    feed.load(function (data) {
+	        
+	        if(getCookie('notify-closed')!=='true'){
+				var entry = data.feed.entries[0];
+				var date = new Date(entry.publishedDate);
+				var formattedDate = date.toLocaleString(navigator.language, {month:'long', day: 'numeric', year:'numeric', hour: '2-digit', minute:'2-digit'});
+				var parser = document.createElement('a');
+				parser.href = entry.link;
+		        
+		        // styles and container
+		        jQuery('body').prepend('<style type="text/css">#notify-container{display:none;background:#fff;box-shadow:0 0 .15em #333;position:relative;top:0;z-index:999;line-height:1.25em;padding:7px 0;}#notify-icon-close{cursor:pointer;float:right;height:1em;width:auto;position:relative;right:7px;top:7px;}#notify-inner{margin:0 auto;max-width:50em;}#notify-content{padding-left:75px;}#notify-icon-cal{height:60px;width:60px; padding-left:5px; float:left;}#notify-host{font-size:.8em;font-style:italic;color:#777;}#notify-content a{color:inherit;border:none;text-decoration:none;}#notify-date{font-size:.9em;}</style><div id="notify-container"></div>');
+		        
+		        // content
+		        jQuery('#notify-container').html('<div id="notify-inner"><img id="notify-icon-cal" src="/plugins/CalendarFeed/assets/cal.png"><div id="notify-content"><a href="'+entry.link+'" target="_blank"><strong>'+entry.title+'</strong></a><br><span id="notify-date">'+formattedDate+'</span><br><span id="notify-host">Learn more at <a style="" href="'+entry.link+'" target="_blank">'+parser.hostname+'</a></span></div></div>');
+		        
+		        // doing some tricks to get the height to work well with the animation
+		        var computedHeight=jQuery('#notify-container').height();
+		        jQuery('#notify-container').css("height",computedHeight).prepend('<img alt="close event notification" id="notify-icon-close" src="/plugins/CalendarFeed/assets/close.png">').slideDown('slow','swing');
+		        
+		        // close button
+				jQuery('#notify-icon-close').click(function(e){
+					var container=document.getElementById(this.parentElement.id);
+					jQuery(container).slideUp('fast','swing');
+					setCookie('notify-closed','true',1);
+		
+				});
+	        }else{
+		        console.log('Notifications dismissed for one day.');
+	        }
+	        
+	    });
+	});
+	</script>
+	<?php
+
+	if(!$show_mobile){ ?>
+	
+	<style type="text/css">
+	@media all and (max-width:<?php echo $breakpoint;?>px){
+		#notify#calendar{display:none !important;}
+	}
+	</style>
+	<?php }
 
 }
