9156df1b09395c75e1f6a2c02d9d4cee672e9da8
Erin Bell <euclid@Erin-Bells-MacBook-Pro.local>
2015-11-24 17:51:01 -0500

    adds plugin shortcode

--- CalendarFeedPlugin.php
+++ CalendarFeedPlugin.php
@@ -8,6 +8,7 @@ class CalendarFeedPlugin extends Omeka_Plugin_AbstractPlugin
 		'config_form',
 		'config',
 		'public_footer',
+		'initialize',
 	);
 
 
@@ -62,6 +63,21 @@ class CalendarFeedPlugin extends Omeka_Plugin_AbstractPlugin
 		$this->_uninstallOptions();
 
 	}
+
+	/**
+	 * Add Shortcode.
+	 */
+
+    public function hookInitialize()
+    {
+        add_shortcode('calendarfeed', array($this, 'cf_embedCalendarFeed'));
+    }
+
+    public function cf_embedCalendarFeed($args, $view)
+    {
+        return '<div id="cf-upcoming-container" style="display:none;"><div id="cf-upcoming-header"><img src="/plugins/CalendarFeed/assets/cal-trans.png"><h3>Upcoming Events</h3><div id="cf-upcoming-feed-link"><span></span></div></div><div id="cf-upcoming-inner"><ul></ul></div></div>';
+    }	
+	
 }
 
 function cf_footerScripts()
@@ -134,7 +150,21 @@ function cf_calendarNotifications($feed)
 	jQuery(jQuery(window)).load(function(){
 	    var feed = new google.feeds.Feed('<?php echo $feed;?>');
 	    feed.load(function (data) {
-	        
+
+	        // styles and notification container
+	        jQuery('body').prepend('<style type="text/css">#cf-notify-container{display:none;background:#fff;box-shadow:0 0 .15em #333;position:relative;top:0;z-index:999;line-height:1.25em;padding:7px 0;}#cf-notify-icon-close{cursor:pointer;float:right;height:1em;width:auto;position:relative;right:7px;top:0px;}#cf-notify-inner{margin:0 auto;max-width:50em;}#cf-notify-content{padding-left:75px;}#cf-notify-icon-cal{height:60px;width:60px; padding-left:5px; float:left;}#cf-notify-host{font-size:.8em;font-style:italic;color:#777;}#cf-notify-content a{color:inherit;border:none;text-decoration:none;}#cf-notify-date{font-size:.9em;}#cf-upcoming-container{background: #eaeaea;padding: 0.5em;border-radius: .25em;}#cf-upcoming-container h3{}#cf-upcoming-inner{}#cf-upcoming-feed-link{color: #777;font-size: .9em;font-style: italic;}#cf-upcoming-feed-link a{color:inherit;}#cf-upcoming-container a{border:none;text-decoration:none;}#cf-upcoming-container h3{margin:0;}#cf-upcoming-header{}#cf-upcoming-header h3,#cf-upcoming-feed-link{padding-left:60px;}#cf-upcoming-header img{width:50px;height:auto;float:left;}  </style><div id="cf-notify-container"></div>');
+		    
+		    // calendar embed
+		    jQuery.each(data.feed.entries,function(e,event){
+			   console.log(event); 
+			   jQuery('#cf-upcoming-inner ul').append('<li><a href="'+event.link+'">'+event.title+'</a><br><span>'+formatDate(event.publishedDate)+'</span></li>');
+		    });
+		    jQuery('#cf-upcoming-feed-link span').html('via: <a href="'+data.feed.link+'">'+hostname(data.feed.link))+'</a>';
+		    jQuery('#cf-upcoming-container').fadeIn();
+		    
+		    
+		    
+		    // event notification 	        
 	        if(getCookie('cf-notify-closed')!=='true'){
 		        
 				var entry = data.feed.entries[0];
